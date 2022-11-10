7695ad77bccbe824200fc114a1e36a6d91d3cebe
harshalpatil655 <harshalpatil655@gmail.com>
2022-11-10 19:06:10 +0530

    Addednode

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const { authetication } = require("./Middlewares/Authetication");
 const { todoController } = require("./Routes/Todos.Route");
 
 const { userController } = require("./Routes/User.Route");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/", (req, res) => {
   console.log("HomePage");
